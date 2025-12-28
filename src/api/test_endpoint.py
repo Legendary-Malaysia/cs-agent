14ca7c5e1bfd9660e951c44a807f9c058a7391fd
kvpratama <kvpratama@gmail.com>
2025-12-28 18:51:10 +0900

    Update test_endpoint.py

--- src/api/test_endpoint.py
+++ src/api/test_endpoint.py
@@ -5,12 +5,13 @@ import sys
 import os
 import pytest
 import json
-import httpx
 
 # Add the 'src' directory to the Python path to resolve csagent module
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-from csagent.configuration import Configuration
 
+from dotenv import load_dotenv
+
+load_dotenv()
 BASE_URL = os.getenv("BASE_URL")
 
 client = TestClient(app)
@@ -35,49 +36,6 @@ def test_health_check_deployed():
 
 
 @pytest.mark.skip(reason="Run this test manually")
-def test_run_supervisor():
-    """Tests the supervisor endpoint."""
-    config = {
-        "configurable": {
-            "thread_id": "test_thread_id",
-            "language": "en",
-            "model": "LongCat-Flash-Chat",
-        }
-    }
-    response = client.post(
-        "/supervisor",
-        json={"messages": "Where can I buy spirit?", "config": config},
-    )
-    print("################# response: ", response)
-    print("################# response.json(): ", response.json()["response"])
-    # assert response.status_code == 200
-    # You might want to add more specific assertions here based on the expected output
-    # assert isinstance(response.json(), dict)
-
-
-@pytest.mark.skip(reason="Run this test manually")
-async def test_run_supervisor_stream():
-    """Tests the supervisor endpoint."""
-
-    async with httpx.AsyncClient() as client:
-        async with client.stream(
-            "POST",
-            "http://localhost:8000/supervisor",
-            json={
-                "messages": "Where can I buy spirit in Genting?",
-                "config": Configuration().model_dump(),
-            },
-            timeout=30.0,
-        ) as response:
-            # if response.status_code == 200:
-            async for line in response.aiter_lines():
-                if line and line.startswith("data: "):
-                    json_str = line.replace("data: ", "")
-                    data = json.loads(json_str)
-                    print(f"Node: {data['node']} | Content: {data['content']}")
-
-
-@pytest.mark.skip(reason="Run this test manually")
 def test_run_supervisor_stream_conversation():
     url = "http://127.0.0.1:8000/supervisor"
     payload = {
@@ -88,7 +46,12 @@ def test_run_supervisor_stream_conversation():
                 "content": "I'm doing well, thank you! How can I help you today?",
             },
             {"role": "user", "content": "What is my name?"},
-        ]
+        ],
+        "config": {
+            "thread_id": "test_thread_id",
+            "language": "en",
+            "model": "LongCat-Flash-Chat",
+        },
     }
 
     try:
@@ -108,35 +71,7 @@ def test_run_supervisor_stream_conversation():
         print(f"Error: {e}")
 
 
-@pytest.mark.skip(reason="Run this test manually")
-def test_run_supervisor_deployed():
-    """Tests the supervisor endpoint on deployed app."""
-    config = {
-        "configurable": {
-            "thread_id": "test_thread_id",
-            "language": "en",
-            "model": "LongCat-Flash-Chat",
-        }
-    }
-    response = requests.post(
-        f"{BASE_URL}/supervisor",
-        json={
-            "messages": [{"role": "user", "content": "Where can I buy spirit?"}],
-            "config": config,
-        },
-    )
-    print("################# response: ", response)
-    print("################# response.json(): ", response.json()["response"])
-
-
 if __name__ == "__main__":
     # test_health_check()
     # test_health_check_deployed()
-    # test_run_supervisor()
-    # test_run_supervisor_deployed()
-    # test_run_supervisor_stream()
-
-    # import asyncio
-    # asyncio.run(test_run_supervisor_stream())
-
     test_run_supervisor_stream_conversation()
