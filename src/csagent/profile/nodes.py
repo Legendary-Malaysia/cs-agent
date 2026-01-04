0103489d0b2fde189f5cc12ec1bac66a20c0d24b
kvpratama <kvpratama@gmail.com>
2026-01-04 12:42:29 +0900

    Add Model Fallback Middleware

--- src/csagent/profile/nodes.py
+++ src/csagent/profile/nodes.py
@@ -5,6 +5,8 @@ from langchain.chat_models import init_chat_model
 from langgraph.runtime import Runtime
 from langchain_core.messages import HumanMessage
 from langgraph.config import get_stream_writer
+from langchain.agents import create_agent
+from langchain.agents.middleware import ModelFallbackMiddleware
 
 from csagent.configuration import Configuration, get_model_info
 from csagent.profile.state import ProfileWorkflowState
@@ -15,11 +17,12 @@ logger = logging.getLogger(__name__)
 def profile_team_node(state: ProfileWorkflowState, runtime: Runtime[Configuration]):
     logger.info("Call profile team")
     writer = get_stream_writer()
-    writer({"custom_key": "Unveiling our fragrance..."})
+    writer({"custom_key": "The character settles into harmony..."})
 
     try:
         task = state.get("task")
         model_info = get_model_info(runtime.context.model_medium)
+        model_info_small = get_model_info(runtime.context.model_small)
 
         current_dir = Path(__file__).parent
 
@@ -56,15 +59,30 @@ def profile_team_node(state: ProfileWorkflowState, runtime: Runtime[Configuratio
 
         llm = init_chat_model(
             **model_info,
-            temperature=0,
+            temperature=0.5,
+        )
+        llm_small = init_chat_model(
+            **model_info_small,
+            temperature=0.5,
+        )
+        agent = create_agent(
+            model=llm,
+            tools=[],
+            name="profile_agent",
+            middleware=[
+                ModelFallbackMiddleware(
+                    llm_small,
+                )
+            ],
         )
 
-        response = llm.invoke(messages)
+        response = agent.invoke(
+            {"messages": messages},
+            config={"tags": ["profile_team"]},
+        )
 
-        logger.info(f"Response from profile team: {response.content}")
-
-        response.name = "profile_team"
-        return {"response": response.content}
+        logger.info(f"Response from profile team: {response['messages'][-1].content}")
+        return {"response": response["messages"][-1].content}
     except Exception:
         logger.exception("Error in profile team node")
         return {"response": "Unexpected error occurred. Please try again later."}
